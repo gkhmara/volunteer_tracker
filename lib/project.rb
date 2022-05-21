7d07956774f8d2dad2c2ab8db53524eb1f7e5b23
Greg Khmara <gkhmara@gmail.com>
2022-05-21 15:04:48 -0700

    add == and all methods

--- lib/project.rb
+++ lib/project.rb
@@ -7,4 +7,19 @@ class Project
     @id = attributes.fetch(:id)
   end
 
+  def ==(project_to_compare)
+    self.title() == project_to_compare.title()
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
 end
