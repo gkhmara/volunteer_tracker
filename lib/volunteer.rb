754512ebaff01cdc217592bd108f50c2a8849261
Greg Khmara <gkhmara@gmail.com>
2022-05-21 16:10:53 -0700

    add initialize and == methods

---
+++ lib/volunteer.rb
@@ -0,0 +1,15 @@
+class Volunteer
+  attr_reader :id
+  attr_accessor :name, :project_id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+  def ==(volunteer_to_compare)
+    (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+  end
+
+end
