2fa094556df2b8db9bcdd117f3c19d12ae4c8a10
Greg Khmara <gkhmara@gmail.com>
2022-05-22 10:45:45 -0700

    add all routes to app.rb

--- app.rb
+++ app.rb
@@ -21,3 +21,61 @@ end
 get('/projects/new') do
   erb(:new_album)
 end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+
+post('/projects') do
+  title = params[:project_title]
+  project = Project.new(title, nil)
+  project.save()
+  @projects = Project.all()
+  erb(:projects)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[:title])
+  @projects = Project.all
+  erb(:projects)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer = Volunteer.find(params[:volunteer_id]).to_i())
+  erb(:volunteer)
+end
+
+post('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.new(params[:volunteer_name], @project.id, nil)
+  volunteer.save()
+  erb(:project)
+end
+
+patch('/projects/:id/volunteers/:volunteer_id') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.update(params[:name], @project.id)
+  erb(:project)
+end
+
+delete('/projects/:id/volunteers/:volunteer_id') do
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.delete
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
