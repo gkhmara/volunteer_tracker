ead3319b51650c8c42a3c299c2871397a5b1d4d4
Greg Khmara <gkhmara@gmail.com>
2022-05-22 11:53:57 -0700

    bug fix

--- lib/project.rb
+++ lib/project.rb
@@ -34,11 +34,9 @@ class Project
     Project.new({:title => title, :id => id})
   end
 
-  def update(attributes)
-    if (attributes.has_key?(:title)) && (attributes.fetch(:title) != nil)
-      @title = attributes.fetch(:title)
+  def update(title)
+      @title = title
       DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
-    end
   end
 
   def delete
