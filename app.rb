f0c3d0f1beb5b6718402ceee86bbc15c9adbaa2c
Greg Khmara <gkhmara@gmail.com>
2022-05-30 15:08:45 -0700

    fix error

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ get('/projects/:id') do
 end
 
 post('/projects') do
-  title = params[:title]
+  title = params[:project_name]
   project = Project.new({:title => title, :id => nil})
   project.save()
   @projects = Project.all()
